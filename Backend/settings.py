2c4f7d724f5552fa1f6d4bc81a74873a90522b94
shangugregory <shangugregory@gmail.com>
2024-01-18 22:59:13 +0300

    vercel.json

--- Backend/settings.py
+++ Backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jd75%xz1tb*cx!u#-2f!9x#123vxb*a=(eruvis*bl583!^cw1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
